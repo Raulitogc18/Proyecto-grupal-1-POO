0b94208a49cf98e8fb362834d36b7d0e67e74dab
juancarlos200 <jk6610687@gmai.com>
2025-10-15 20:24:14 -0600

    mas cambios

--- main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Solo lanza la interfaz gráfica
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new VistaGUI();
-        });
-    }
-}
